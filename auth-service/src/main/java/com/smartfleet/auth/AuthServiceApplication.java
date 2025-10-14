2fa5a62628898d7f524d1103fa30301416d32301
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-13 21:35:59 -0400

    Base project setup - auth service

---
+++ auth-service/src/main/java/com/smartfleet/auth/AuthServiceApplication.java
@@ -0,0 +1,8 @@
+package com.smartfleet.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class AuthServiceApplication {
+}
