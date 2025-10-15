8e9bc9f15712c0130c0617d4c918550f898b91d9
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-14 23:41:15 -0400

    refresh token table creation

--- auth-service/src/main/java/com/smartfleet/auth/model/RefreshToken.java
+++ auth-service/src/main/java/com/smartfleet/auth/model/RefreshToken.java
@@ -1,4 +1,11 @@
 package com.smartfleet.auth.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "refresh_tokens")
 public class RefreshToken {
 }
