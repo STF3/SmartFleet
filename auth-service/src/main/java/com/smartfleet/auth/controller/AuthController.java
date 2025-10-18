9012722411e14b3b2225bd9390ad374d088b6396
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-18 12:48:57 -0400

    add controllers to auth methods

--- auth-service/src/main/java/com/smartfleet/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/smartfleet/auth/controller/AuthController.java
@@ -1,4 +1,12 @@
 package com.smartfleet.auth.controller;
 
+import com.smartfleet.auth.model.User;
+import com.smartfleet.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+
+
 public class AuthController {
 }
