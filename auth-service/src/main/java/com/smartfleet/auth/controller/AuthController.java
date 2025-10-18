e06d5012549a7ec172bed4419dd8b4df61318479
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-18 12:55:39 -0400

    add controllers to auth methods

--- auth-service/src/main/java/com/smartfleet/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/smartfleet/auth/controller/AuthController.java
@@ -7,6 +7,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 
-
+@RestController
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public Map<String, String> register(@RequestBody User user) {
+        return authService.register(user);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> payload) {
+        return authService.login(payload.get("email"), payload.get("password"));
+    }
+
+    @PostMapping("/refresh")
+    public Map<String, String> refresh(@RequestBody Map<String, String> payload) {
+        return authService.refresh(payload.get("refreshToken"));
+    }
 }
