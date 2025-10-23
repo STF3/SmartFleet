fd1071f99d1ec8722cf79aa09a7a27623a15160a
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-10-22 21:25:39 -0400

    added main appfile

---
+++ fleet-service/src/main/java/com/smartfleet/fleet/FleetServiceApplication.java
@@ -0,0 +1,11 @@
+package com.smartfleet.fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FleetServiceApplication.class, args);
+    }
+}
